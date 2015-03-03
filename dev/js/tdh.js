441a29f6c2f04268b5a668ba1cba9c2595e81a7a
afriC84 <colja.carls@gmail.com>
2015-03-03 09:42:11 +0100

    javascript stub

--- dev/js/tdh.js
+++ dev/js/tdh.js
@@ -1,1 +1,21 @@
 /* individuelles JS */
+
+var TDH = {
+    /**
+     * Colja
+     */
+    initAkkordeon: function () {
+        "use strict";
+        // do whatever needed
+    }
+};
+
+/*
+ When DOM is ready
+ */
+$(function () {
+    // Akkordeon
+    if ($('#akkordeon').length > 0) {
+        TDH.initAkkordeon();
+    }
+});
