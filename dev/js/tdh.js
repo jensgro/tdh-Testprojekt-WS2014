3c8b3bf9bd1f283644967bc63213d483fd6bd877
afriC84 <colja.carls@gmail.com>
2015-03-03 13:07:09 +0100

    Akkordeon working

--- dev/js/tdh.js
+++ dev/js/tdh.js
@@ -6,7 +6,28 @@ var TDH = {
      */
     initAkkordeon: function () {
         "use strict";
-        // do whatever needed
+        var speed = 700,
+            akkordeons = $('.akkordeon');
+
+        for (var i = 0; i < akkordeons.length; i++) {
+            var akk = akkordeons[i],
+                $articles = $($(akk).find('article'));
+            
+            $articles.click(function() {
+                var $this = $(this);
+
+                $articles
+                    .filter('.isOpen')
+                    .find('.content')
+                    .slideUp(speed);
+                $articles.removeClass('isOpen');      
+                
+                $this
+                    .find('.content')
+                    .slideDown(speed);
+                $this.addClass('isOpen');
+            });
+        }
     }
 };
 
@@ -15,7 +36,7 @@ var TDH = {
  */
 $(function () {
     // Akkordeon
-    if ($('#akkordeon').length > 0) {
+    if ($('.akkordeon').length > 0) {
         TDH.initAkkordeon();
     }
 });
